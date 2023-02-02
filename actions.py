766c20808867bb1425150121c7511ab605ab41e4
Lucky Boy <bachb0952001@gmail.com>
2023-02-02 13:10:35 +0700

    paraphraser

--- actions.py
+++ actions.py
@@ -21,3 +21,22 @@ class ActionDefaultFallback(Action):
 
         # Revert user message which led to fallback.
         return [UserUtteranceReverted()]
+class ActionShowHelp(Action):
+    
+    def name(self) -> Text:
+        return "action_show_help"
+    
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        list_ques = {
+            "Rút tiền như thế nào",
+            "Làm thế nào khi bị nuốt thể",
+            "Chuyển khoản thế nào",
+        }
+        res = "Đây là một sô câu hỏi tôi có thể trả lời:\n" + "\n".join(list_ques)
+        dispatcher.utter_message(text=res)
+        return []
